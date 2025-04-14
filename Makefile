83e37e57b812f7599a0b651ed5ed724e0352eec5
joongi007 <68692377+joongi007@users.noreply.github.com>
2025-04-15 00:48:46 +0900

    initial mcp resource support (#43)
    ---------
    
    Co-authored-by: vbarda <vadym@langchain.dev>

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ test_watch:
 ######################
 
 # Define a variable for Python and notebook files.
-lint format: PYTHON_FILES=.
+lint format: PYTHON_FILES=langchain_mcp_adapters/ tests/
 lint_diff format_diff: PYTHON_FILES=$(shell git diff --relative=. --name-only --diff-filter=d master | grep -E '\.py$$|\.ipynb$$')
 
 lint lint_diff:
--- langchain_mcp_adapters/client.py
+++ langchain_mcp_adapters/client.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from types import TracebackType
 from typing import Any, Literal, Optional, TypedDict, cast
 
+from langchain_core.documents.base import Blob
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.tools import BaseTool
 from mcp import ClientSession, StdioServerParameters
@@ -11,6 +12,7 @@ from mcp.client.sse import sse_client
 from mcp.client.stdio import stdio_client
 
 from langchain_mcp_adapters.prompts import load_mcp_prompt
+from langchain_mcp_adapters.resources import load_mcp_resources
 from langchain_mcp_adapters.tools import load_mcp_tools
 
 EncodingErrorHandler = Literal["strict", "ignore", "replace"]
@@ -275,6 +277,21 @@ class MultiServerMCPClient:
         """Get a prompt from a given MCP server."""
         session = self.sessions[server_name]
         return await load_mcp_prompt(session, prompt_name, arguments)
+
+    async def get_resources(
+        self, server_name: str, uris: str | list[str] | None = None
+    ) -> list[Blob]:
+        """Get resources from a given MCP server.
+
+        Args:
+            server_name: Name of the server to get resources from
+            uris: Optional resource URI or list of URIs to load. If not provided, all resources will be loaded.
+
+        Returns:
+            A list of LangChain Blobs
+        """
+        session = self.sessions[server_name]
+        return await load_mcp_resources(session, uris)
 
     async def __aenter__(self) -> "MultiServerMCPClient":
         try:
